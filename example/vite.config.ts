4d3f5befb8ed4f616c614e43b193e2987fb6fc65
Jonathan Raftery <jonathan@jonathanraftery.com>
2024-05-07 10:29:26 -0500

    Update example

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {'@wavecx/wavecx-react': '..'},
-    preserveSymlinks: true,
-  },
-  optimizeDeps: {include: ['@wavecx/wavecx-react']},
 })
